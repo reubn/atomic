6d5ad2e75448919ed4f87714c1c95b9b992f3e8e
reuben <reuben.eggar@me.com>
2017-05-22 19:36:36 +0100

    ✨ Toggle Seconds Display

--- src/clockAct/Act.js
+++ src/clockAct/Act.js
@@ -1,25 +1,36 @@
 import moment from 'moment'
 
+import button, {press} from '../button'
+
 import renderText from '../Display/renderText'
 
 class ClockAct {
   constructor(){
     this.timer = null
 
+    this.seconds = false
     this.alternateEvenOdd = 0
   }
 
   start({display}){
     this.render(display)
     this.timer = setInterval(() => this.render(display), 500)
+
+    // Button Events
+    this.buttonHandler = () => this.toggleSeconds()
+    button.on(press, this.buttonHandler)
   }
   end(){
     clearInterval(this.timer)
+    button.removeListener(press, this.buttonHandler)
+    this.seconds = false
     this.alternateEvenOdd = 0
   }
+  toggleSeconds(){this.seconds = !this.seconds}
 
   render(display){
-    display.display2DArray(renderText(moment().format(`h${(this.alternateEvenOdd % 2) ? ':' : ' '}mm`), display.width, display.height, true))
+    const seconds = this.seconds ? `${(this.alternateEvenOdd % 2) ? ':' : ' '}ss` : ''
+    display.display2DArray(renderText(moment().format(`h${(this.alternateEvenOdd % 2) ? ':' : ' '}mm${seconds}`), display.width, display.height, true))
     this.alternateEvenOdd++
   }
 }
