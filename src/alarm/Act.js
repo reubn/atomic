d16dafea4bd2aa655193f7607aae4619bceb6d7f
reuben <reuben.eggar@me.com>
2017-05-28 15:09:40 +0100

    🐛 TTS

--- src/alarm/Act.js
+++ src/alarm/Act.js
@@ -1,3 +1,5 @@
+import moment from 'moment'
+
 import button, {press} from '../button'
 import clockAct from '../clockAct'
 import {renderText} from '../Display'
@@ -11,6 +13,9 @@ class AlarmAct {
 
     this.timer = null
     this.endHandle = null
+
+    // TTS
+    this.ttsPromise = weather().then(summary => tts(`The time is ${moment().format('h:mm; [on] dddd [the] Do [of] MMMM')}. ${summary}`))
 
     this.continuePlaying = false
 
@@ -31,7 +36,7 @@ class AlarmAct {
     this.timer = setInterval(() => this.render(display), 500)
 
     // Button Events
-    button.once(press, () => this.end())
+    button.once(press, () => this.onButtonPress(sound))
   }
   end(fromDisplayManager=false){
     if(!fromDisplayManager) return this.endHandle(clockAct)
@@ -39,6 +44,23 @@ class AlarmAct {
     clearInterval(this.timer)
     this.endHandle = null
     this.continuePlaying = false
+  }
+
+  onButtonPress(sound){
+    this.continuePlaying = false
+
+    const feedbackPromise = new Promise(resolve => {
+      sound.newSource(successSound)
+      sound.on('close', resolve)
+    })
+
+    Promise.all([this.ttsPromise, feedbackPromise])
+    .then(([path]) => new Promise(resolve => {
+      sound.newSource(path, 'local')
+      sound.on('close', resolve)
+    }))
+    .then(() => this.end())
+    .catch(err => console.error(err))
   }
 
   render(display){
