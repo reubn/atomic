fd72c72277dcb91b9aba8ba7723e6177f99600e9
reuben <reuben.eggar@me.com>
2017-05-30 08:38:28 +0100

    🎨 async/await

--- src/alarms/index.js
+++ src/alarms/index.js
@@ -6,22 +6,22 @@ import AlarmAct from '../acts/Alarm'
 
 import Alarm from './Alarm'
 
-const init = () => {
-  Alarm.clearAll()
-  .then(() => {
-    const {hours: hour, minutes: minute, seconds: second} = moment().add(5, 's').toObject()
-    // const [hour, minute, second] = [7, 30, 0]
+const init = async () => {
+  await Alarm.clearAll()
 
-    const testAlarm = new Alarm({
-      name: 'Test 1',
-      scheduleDescriptor: {hour, minute, second}
-    })
+  const {hours: hour, minutes: minute, seconds: second} = moment().add(5, 's').toObject()
+  //const [hour, minute, second] = [7, 30, 0]
 
-    return testAlarm.save()
+  const testAlarm = new Alarm({
+    name: 'Test 1',
+    scheduleDescriptor: {hour, minute, second}
   })
-  .then(Alarm.fromDB)
-  .then(alarms => alarms.map(alarm => alarm.schedule(() => manager.connect(new AlarmAct(alarm)))))
-  .then(scheduledAlarms => console.log('Alarms will trigger at', scheduledAlarms.map(alarm => alarm.scheduleDescriptor)))
+  await testAlarm.save()
+
+  const alarms = await Alarm.fromDB()
+  const scheduledAlarms = alarms.map(alarm => alarm.schedule(() => manager.connect(new AlarmAct(alarm))))
+
+  console.log('Alarms will trigger at', scheduledAlarms.map(alarm => alarm.scheduleDescriptor))
 }
 
 export default init
