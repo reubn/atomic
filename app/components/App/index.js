58070ea5872810f0dee1b1303d8e08597b06f880
reuben <reuben.eggar@me.com>
2017-07-04 21:29:40 +0100

    🐛 Status Bar

--- app/components/App/index.js
+++ app/components/App/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import {StatusBar} from 'react-native'
 import {Provider} from 'react-redux'
 
 import store from '../../store'
 
 import Setup from '../Setup'
 
-export default () => (
-  <Provider store={store}>
-    <Setup />
-  </Provider>
-)
+export default () => {
+  StatusBar.setBarStyle('light-content', true)
+  return (
+    <Provider store={store}>
+      <Setup />
+    </Provider>
+  )
+}
