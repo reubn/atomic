2bc139ad47471dc5b25dd53bf624a42ac3f5a7f1
reuben <reuben.eggar@me.com>
2017-07-23 12:41:52 +0100

    ✨ Expose defaultAct

--- clock/src/Manager/index.js
+++ clock/src/Manager/index.js
@@ -1,6 +1,9 @@
 import ClockAct from '../acts/Clock'
 import Manager from './Manager'
 
-const manager = new Manager({display: [4]}, new ClockAct())
+const defaultAct = new ClockAct()
+
+const manager = new Manager({display: [4]}, defaultAct)
 
 export default manager
+export {defaultAct}
