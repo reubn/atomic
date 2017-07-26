f17083f595658fb26b698c0a7d26dbdd0b2e52f6
reuben <reuben.eggar@me.com>
2017-07-26 13:07:06 +0100

    ✨ Add state structure for auth

--- app/store/initials/setup.js
+++ app/store/initials/setup.js
@@ -1,8 +1,11 @@
+import clockInitial from './clock'
+
 export default {
-  selectedClock: null,
+  selectedClock: clockInitial,
   bonjourScan: {
     results: [],
     lastRun: null
   },
-  authPatterns: Array(4).fill().map(() => Array(4).fill().map(() => Array(4).fill(false)))
+  authPatterns: Array(4).fill().map(() => Array(4).fill().map(() => Array(4).fill(false))),
+  authLoading: null
 }
