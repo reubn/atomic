8542cbb34465ecd545207b9fab2bf0a245059352
reuben <reuben.eggar@me.com>
2017-07-31 10:10:00 +0100

    ✨ Tab Icon

--- app/components/MainContainer/Main/TabController/AlarmsStackController/index.js
+++ app/components/MainContainer/Main/TabController/AlarmsStackController/index.js
@@ -1,7 +1,17 @@
+import React from 'react'
+import {Ionicons} from '@expo/vector-icons'
+
 import CustomStackNavigator from '../../../../CustomStackNavigator'
 
 import AlarmsContainer from './AlarmsContainer'
 
-export default CustomStackNavigator({
-  Alarms: {screen: AlarmsContainer}
-})
+export default class extends CustomStackNavigator({
+  Alarms: {screen: AlarmsContainer},
+}) {
+  static navigationOptions = {
+    tabBarLabel: 'Alarms',
+    tabBarIcon: ({focused, tintColor}) => (
+      <Ionicons name={focused ? 'ios-notifications' : 'ios-notifications-outline'} style={{fontSize: 30, color: tintColor, marginTop: 6}} />
+    )
+  }
+}
