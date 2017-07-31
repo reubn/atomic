fa2af4ca518eaad6f33dc544d06bced3823aeea2
reuben <reuben.eggar@me.com>
2017-07-31 10:08:09 +0100

    🎨 Better Naming

--- app/components/CustomStackNavigator/Header.js
+++ app/components/CustomStackNavigator/Header.js
@@ -3,20 +3,20 @@ import {Button} from 'react-native'
 
 import {headerButton} from './styles'
 
-export const leftButton = (props, {navigation: {state: {params: {leftButton: {active, handler, buttonProps}={}}={}}={}}}=props) => (
+export const leftButton = (propsAll, {navigation: {state: {params: {leftButton: {active, handler, props}={}}={}}={}}}=propsAll) => (
   <Button
     title="Back"
     color={active ? headerButton.color : 'transparent'}
-    onPress={() => handler && active && handler(props)}
-    {...buttonProps}
+    onPress={() => handler && active && handler(propsAll)}
+    {...props}
   />
 )
 
-export const rightButton = (props, {navigation: {state: {params: {rightButton: {active, handler, buttonProps}={}}={}}={}}}=props) => (
+export const rightButton = (propsAll, {navigation: {state: {params: {rightButton: {active, handler, props}={}}={}}={}}}=propsAll) => (
   <Button
     title="Next"
     color={active ? headerButton.color : 'transparent'}
-    onPress={() => handler && active && handler(props)}
-    {...buttonProps}
+    onPress={() => handler && active && handler(propsAll)}
+    {...props}
   />
 )
